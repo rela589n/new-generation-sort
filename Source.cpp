b3ddaf1eb3cb305f31d23fe1cd1cead3848414f5
rela589n <zsidelnik@gmail.com>
2019-12-27 15:16:18 +0200

    Finally fixed memory problems.

--- Source.cpp
+++ Source.cpp
@@ -100,7 +100,7 @@ double getCPUTime()
 using namespace std;
 
 int* glue(int* a, int lenA, int* b, int lenB);
-void newGenerationSort(int* arr, int len);
+void newGenerationSort(int*& arr, int len);
 
 void printArr(int* arr, int len) {
 	for (int i = 0; i < len; ++i) {
@@ -126,11 +126,13 @@ int main() {
 
 	int* a1 = new int[length];
 	int* a2 = new int[length];
+	int* a3 = new int[length];
 
 	for (i = 0; i < length; i++) {
 		//cin >> a[i];
 		a1[i] = rand();
 		a2[i] = a1[i];
+		a3[i] = a1[i];
 		//cout << a[i] << '\t';
 	}
 
@@ -148,34 +150,19 @@ int main() {
 
 	if (!arrsEqual(a1, a2, length)) {
 		cout << "Something wrong with sort!" << endl;
+		cout << "Source arr: ";
+		printArr(a3, length);
+		cout << endl;
+
+		printArr(a1, length);
+		printArr(a2, length);
 	}
-	//printArr(a1, length);
 
 	system("pause");
 	return 0;
 }
 
-void merge2ArraysIntoSingle(int* arr, int len, int& i, int* arr1, int index1, int len1, int* arr2, int index2, int len2) {
-	for (; i < len; ++i) {
-		if (index1 >= len1) {
-			while (index2 < len2) {
-				arr[i++] = arr2[index2++];
-			}
-			break;
-		}
-
-		if (index2 >= len2) {
-			while (index1 < len1) {
-				arr[i++] = arr1[index1++];
-			}
-			break;
-		}
-
-		arr[i] = (arr1[index1] < arr2[index2]) ? arr1[index1++] : arr2[index2++];
-	}
-}
-
-void newGenerationSort(int* arr, int len) {
+void newGenerationSort(int*& arr, int len) {
 	if (len < 2)
 		return;
 	
@@ -203,69 +190,16 @@ void newGenerationSort(int* arr, int len) {
 
 		restElements[restLen++] = arr[i];
 	}
+	delete[] arr; // to fix memory overflow =>>>
+	int* MinMax = glue(minElements + len - minLen, minLen, maxElements, maxLen);
+	
+	delete[] minElements;
+	delete[] maxElements;
 
 	newGenerationSort(restElements, restLen);
 
-	// glue 3 arrays into single
-	int indexMax = 0;
-	int indexRest = 0;
-	int indexMin = len - minLen;
-
-	for (int i = 0; i < len; ++i) {
-		if (indexMax >= maxLen) {
-			// work with min and rest arrays
-
-			merge2ArraysIntoSingle(arr, len, i, restElements, indexRest, restLen, minElements + len - minLen, indexMin - len + minLen, minLen);
-
-			break;
-		}
-
-		if (indexMin >= len) {
-			// work with max and rest arrays
-
-			merge2ArraysIntoSingle(arr, len, i, maxElements, indexMax, maxLen, restElements, indexRest, restLen);
-
-			break;
-		}
-
-		if (indexRest >= restLen) {
-			// work with min and max arrays
-
-			merge2ArraysIntoSingle(arr, len, i, maxElements, indexMax, maxLen, minElements + len - minLen, indexMin - len + minLen, minLen);
-			
-			break;
-		}
-
-		// work with 3 arrays
-
-		int a = minElements[indexMin];
-		int b = maxElements[indexMax];
-		int c = restElements[indexRest];
-
-		if (a < b) {
-			if (a < c) {
-				arr[i] = a;
-				++indexMin;
-			}
-			else { // c < a < b
-				arr[i] = c;
-				++indexRest;
-			}
-		}
-		else if (b < c) { // && b < a
-			arr[i] = b;
-			++indexMax;
-		}
-		else {// c < b < a
-			arr[i] = c;
-			++indexRest;
-		}
-	}
-	
+	arr = glue(MinMax, minLen + maxLen, restElements, restLen); // =>>> we need to delete the rest after significant data
 	delete[] restElements;
-	delete[] minElements;
-	delete[] maxElements;
-	
 }
 
 // work with min and rest arrays
@@ -278,19 +212,18 @@ void newGenerationSort(int* arr, int len) {
 int* glue(int* a, int lenA, int* b, int lenB) {
 	int lenC = lenA + lenB, * c = new int[lenC], indx_a = 0, indx_b = 0, i = 0;
 	for (; i < lenC; i++) {
-		if (indx_a < lenA)
-			if (indx_b < lenB)
+		if (indx_a < lenA) {
+			if (indx_b < lenB) {
 				c[i] = (a[indx_a] < b[indx_b]) ? a[indx_a++] : b[indx_b++];
-			else {
-				while (indx_a < lenA)
-					c[i++] = a[indx_a++];
-				break;
+				continue;
 			}
-		else {
+			while (indx_a < lenA)
+				c[i++] = a[indx_a++];
+		}
+		else
 			while (indx_b < lenB)
 				c[i++] = b[indx_b++];
-			break;
-		}
+		break;
 	}
 	return c;
 }
